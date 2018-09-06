7c699cb8c0ce78167ceccacd92c4415638cfe25d
Bunmi Mise <bunmi_mise@yahoo.com>
2018-09-06 13:51:59 -1000

    refactor: mapbox token removed, server port number replaced

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiYnVubWltaXNlIiwiYSI6ImNqbGRicWM1djA5bmczdmxzdmUwdGcydDQifQ.aLvwMSzTYofMnxuJJ3oZSQ',
+    mapboxToken: 'your MAPBOX API KEY HERE',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
