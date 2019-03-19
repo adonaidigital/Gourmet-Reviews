c89ff20cc72f5daaa7403143873f1ad87f1ebe59
Bunmi Mise <33058548+adonaidigital@users.noreply.github.com>
2019-03-18 22:50:02 -1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiYnVubWltaXNlIiwiYSI6ImNqdGZkMmswYzA4Ymk0YWxsYW1sZmR5YTgifQ.6ZhsD9h1tC0Se9gl05PxCA',
+    mapboxToken: 'MAPBOX TOKEN HERE',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
